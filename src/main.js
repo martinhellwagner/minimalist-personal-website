11fd6eafcbbd6c97c321bdeaf758fb780a57f4b7
Martin Hellwagner <martin@fredmansky.at>
2018-09-24 21:39:48 +0200

    Implement random placement of beach ball

--- src/main.js
+++ src/main.js
@@ -3,6 +3,26 @@ import App from './App.vue';
 
 Vue.config.productionTip = false;
 
-new Vue({
+var app = new Vue({
   render: h => h(App),
-}).$mount('#app');
+  mounted() {
+    this.placeBeachBall();
+    document.addEventListener('click', this.placeBeachBall);
+  },
+  methods: {
+    placeBeachBall: () => {
+      const beachBall = document.querySelector('.beach-ball');
+    
+      const x = document.body.scrollHeight - (beachBall.height / 2);
+      const y = document.body.scrollWidth  - (beachBall.width / 2);
+      const randomX = Math.floor(Math.random() * x);
+      const randomY = Math.floor(Math.random() * y);
+    
+      beachBall.style.top  = randomX + 'px';
+      beachBall.style.left = randomY + 'px';
+      beachBall.style.display = 'block';
+    }
+  },
+});
+
+app.$mount('#app');
