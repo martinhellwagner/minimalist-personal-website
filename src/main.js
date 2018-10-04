3b113fbfe029aafa3311cad96a85d6ee71a06d67
Martin Hellwagner <martin@fredmansky.at>
2018-10-04 21:43:56 +0200

    Slightly change placing of beach ball

--- src/main.js
+++ src/main.js
@@ -47,8 +47,8 @@ const app = new Vue({
     placeBeachBall: () => {
       const beachBall = document.querySelector('.beach-ball');
 
-      const x = document.documentElement.clientHeight - beachBall.height;
-      const y = document.documentElement.clientWidth - beachBall.width;
+      const x = document.documentElement.clientHeight - (beachBall.height * 2);
+      const y = document.documentElement.clientWidth - (beachBall.width * 2);
       const randomX = Math.floor(Math.random() * x);
       const randomY = Math.floor(Math.random() * y);
 
